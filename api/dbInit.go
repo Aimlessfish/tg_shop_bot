ebbaa73b363c90f96b94070b870e58afb55acee6
Aimlessfish <millweekin@gmail.com>
2025-01-01 00:25:54 +0000

    created `dbInit.go`

--- api/dbInit.go
+++ api/dbInit.go
@@ -1,2 +1,45 @@
-package tgshopdb
+package api
 
+import (
+	"database/sql"
+	"fmt"
+	"log/slog"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+func dbConnect() (*sql.DB, error) {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+	logger = slog.With("LogID", "API: dbConnect() ")
+
+	err := godotenv.Load()
+	if err != nil {
+		logger.Warn("Error", "loading .env file", err.Error())
+	}
+
+	// Get database credentials from environment variables
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	logger.Info("Connected to the database.")
+	return db, nil
+
+}
